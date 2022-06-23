c2877197beb14fc6b181510a7ab050d30fc96df5
preet290 <preetpatel@2992000@gmail.com>
2022-06-23 09:57:39 +0530

    second commi

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,20 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(),
+    String name = "preet";
+    int id = 1;
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Name is : $name and Id is : $id"),
+        ),
+      ),
     );
   }
 }
